c450bbf4a709d6c19684b6bb9b1507f9d0fc240a
Arkadii Lantukh <lan18029@byui.edu>
2022-04-22 15:04:04 +0300

    Final commit of week 1

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyFirstConsoleApplication
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Call the method to create an instance of a person
+            GetUserNameAndLocation();
+            // Call the X-mass countdown method
+            ChristmasCountdown();
+            // Call the Glazer class with its method
+            GlazerApp.RunExample();
+            // exit the program
+            Console.WriteLine("Press any key to exit...");
+            _ = Console.ReadLine();
         }
+
+        private static void GetUserNameAndLocation()
+        {
+            Console.WriteLine("What is your name? ");
+            var personName = Console.ReadLine();
+            Console.WriteLine($"Hi {personName}! Where are you from? ");
+            var personLocation = Console.ReadLine();
+            Console.WriteLine($"I have never been to {personLocation}. I bet it is nice. Press any key to continue...");
+            // exit
+            Console.ReadLine();
+
+            var person = new Person
+            {
+                Name = personName,
+                Location = personLocation
+            };
+        }
+        private static void ChristmasCountdown()
+        {
+            // method calculates the number of days before X-mas this year.
+            var currentDate = DateTime.Now.Date;
+            Console.WriteLine($"Today's date is: {currentDate.ToString("dd/MM/yyyy")}");
+            // this year date value is static due to the instructions
+            var xmas = DateTime.Parse("2022, 12, 25");
+            var daysUntilX = (xmas - currentDate).Days;
+            Console.WriteLine($"There are {daysUntilX} days until Christmas");
+            // exiting
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+
     }
 }
