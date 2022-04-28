34e11caed2428440fb662f9db870bb554714e383
Arkadii Lantukh <lan18029@byui.edu>
2022-04-28 17:50:14 +0300

    before customization

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -154,5 +154,17 @@ namespace MathQuiz
                 startButton.Enabled = true;
             }
         }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
     }
 }
